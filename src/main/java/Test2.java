1d495801829c43ba888c3fcdb8abc038b001533b
anushkalowanshi <anushkalowanshi6292Agmail.com>
2026-02-17 14:15:52 +0530

    commiting test2 to branchex

--- src/main/java/Test2.java
+++ src/main/java/Test2.java
@@ -1,9 +1,9 @@
 
-public class Test1 {
+public class Test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hii...");
+		System.out.println("Hello...");
 
 	}
 
